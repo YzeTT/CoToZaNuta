bb757077b76227dd08edc00b9e750d8b7f33e6d1
YzeTT <dawlas@st.amu.edu.pl>
2021-11-21 14:48:05 +0100

    first part of app

---
+++ CoToZaNuta/Assets/_Project/Scripts/MenuController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject logo;
+    [SerializeField] private ButtonView playButton;
+    [SerializeField] private GameObject categoryPanel;
+
+    private void Start()
+    {
+        InitPlayButton();
+    }
+
+    private void InitPlayButton()
+    {
+        playButton.SetClickAction(MoveToPickingCategory);
+    }
+    
+    private async void MoveToPickingCategory()
+    {
+        logo.SetActive(false);
+        playButton.gameObject.SetActive(false);
+        await Task.Delay(40);
+        EnableCategoryPanel();
+    }
+
+    private void EnableCategoryPanel()
+    {
+        categoryPanel.SetActive(true);
+    }
+
+    private void DisableCategoryPanel()
+    {
+        categoryPanel.SetActive(false);
+    }
+
+    public async void MoveToSplashScreen()
+    {
+        DisableCategoryPanel();
+        await Task.Delay(40);
+        logo.SetActive(true);
+        playButton.gameObject.SetActive(true);
+    }
+}
