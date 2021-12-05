63f24a05024d4756551439e071ab5f14c4dd1a4d
YzeTT <dawlas@st.amu.edu.pl>
2021-12-05 15:44:23 +0100

    nvm

--- CoToZaNuta/Assets/Scripts/Editor/JenkinsBuild.cs
+++ CoToZaNuta/Assets/Scripts/Editor/JenkinsBuild.cs
@@ -6,7 +6,7 @@ public class JenkinsBuild
     static void PerformBuild()
     {
         string[] defaultScene = {"Assets/_Project/Scenes/MainScene.unity"};
-        BuildPipeline.BuildPlayer(defaultScene, "./game.x86_64",
+        BuildPipeline.BuildPlayer(defaultScene, "./builds/game.x86_64",
             BuildTarget.Android, BuildOptions.None);
     }
 }
