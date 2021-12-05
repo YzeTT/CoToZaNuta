584cd816160364dcc22fec05f96f6829016ca21a
YzeTT <dawlas@st.amu.edu.pl>
2021-12-05 12:33:54 +0100

    try to make jenkins error

---
+++ CoToZaNuta/Assets/_Project/Scripts/Jenkins/BuildScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+public class BuildScript
+{
+    static void PerformBuild()
+    {
+        string[] defaultScene = {"Assets/_Project/Scenes/MainScene.unity"};
+        BuildPipeline.BuildPlayer(defaultScene, "./builds/game.x86_64",
+            BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+}
+    
